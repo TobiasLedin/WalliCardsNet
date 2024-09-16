f14f9cfaf9a992789c362163b5a8f917c3e51023
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-16 11:26:50 +0200

    Added seeding of roles to ApplicationDbContext. Added role constants.

---
+++ WalliCardsNet.API/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace WalliCardsNet.API.Constants
+{
+    public static class Roles
+    {
+        public const string User = "User";
+
+        public const string Administrator = "Admininistrator";
+    }
+}
