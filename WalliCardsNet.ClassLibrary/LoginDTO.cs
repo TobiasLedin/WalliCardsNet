bebcf3e50a51dbcaf33ac469c73aadfa1ed41913
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-17 00:25:48 +0200

    Updated DTO example.

--- WalliCardsNet.ClassLibrary/LoginDTO.cs
+++ WalliCardsNet.ClassLibrary/LoginDTO.cs
@@ -1,7 +1,16 @@
-﻿namespace WalliCardsNet.ClassLibrary
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalliCardsNet.ClassLibrary
 {
     public class LoginDTO
     {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
 
+        [Required]
+        [PasswordPropertyText]
+        public required string Password { get; set; }
     }
 }
