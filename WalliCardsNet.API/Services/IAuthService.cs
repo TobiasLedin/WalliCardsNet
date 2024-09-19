691e49ec9f04645a9acdac36b319107da9b43ffa
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-19 11:08:20 +0200

    Updating AuthenticationController to work with record DTO's. Changed name of AuthService -> APIAuthService to distinct from ClientAuthService.

--- WalliCardsNet.API/Services/IAuthService.cs
+++ WalliCardsNet.API/Services/IAuthService.cs
@@ -1,12 +1,13 @@
-﻿using static WalliCardsNet.API.Services.AuthService;
+﻿using WalliCardsNet.ClassLibrary;
+using static WalliCardsNet.API.Services.APIAuthService;
 
 namespace WalliCardsNet.API.Services
 {
     public interface IAuthService
     {
 
-        Task<RegisterResult> RegisterEmployeeAsync(string name, string email, string password);
+        Task<RegisterResultDTO> RegisterEmployeeAsync(string name, string email, string password);
 
-        Task<LoginResult> LoginAsync(string email, string password);
+        Task<LoginResultDTO> LoginAsync(string email, string password);
     }
 }
--- WalliCardsNet.ClassLibrary/RegisterRequestDTO.cs
+++ WalliCardsNet.ClassLibrary/RegisterRequestDTO.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace WalliCardsNet.ClassLibrary
 {
-    public record LoginResultDTO(bool Success, string? Token, string? Details);
+    public record RegisterRequestDTO(string FormDataId, string UserName, string Password);
     
 }
