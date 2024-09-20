e44d4595be85ef3994b0fd07ffae14693f110403
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-21 01:38:50 +0200

    Updated model.

--- WalliCardsNet.API/Data/Models/Business.cs
+++ WalliCardsNet.API/Data/Models/Business.cs
@@ -4,13 +4,13 @@ namespace WalliCardsNet.API.Data.Models
 {
     public class Business
     {
-        public Guid Id { get; set; } 
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = "";
         public string PspId { get; set; } = "";
         public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
-        public List<CardTemplate>? CardTemplates { get; set; }
-        public List<Customer>? Customers { get; set; }
-        public List<string>? CustomerDetailsJson { get; set; }
+        public List<CardTemplate> CardTemplates { get; set; } = [];
+        public List<Customer> Customers { get; set; } = [];
+        public List<string> CustomerDetailsJson { get; set; } = [];
 
         public List<ApplicationUser> ApplicationUsers { get; set; } = [];
     }
