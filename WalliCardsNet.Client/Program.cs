5498dbfbf6f30e2e594d62908b3e92cf0fcc392a
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-20 09:43:57 +0200

    Added client-side authentication settings.

--- WalliCardsNet.Client/Program.cs
+++ WalliCardsNet.Client/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WalliCardsNet.Client.Handlers;
 
 namespace WalliCardsNet.Client
 {
@@ -8,13 +10,26 @@ namespace WalliCardsNet.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddBlazoredLocalStorage();
+
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient
-            { 
-                BaseAddress = new Uri("https://localhost:7204") 
+            {
+                BaseAddress = new Uri("https://localhost:7204")
+                
             });
+
+            // TEST (HttpClientFactory)
+            builder.Services.AddHttpClient("wallicardAPI", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7204");
+            })
+            .AddHttpMessageHandler<AuthTokenHandler>();
+
+            //builder.Services.AddAuthorizationCore
 
             await builder.Build().RunAsync();
         }
