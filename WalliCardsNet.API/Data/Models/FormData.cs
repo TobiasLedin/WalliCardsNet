2fe40351043b8fa71b24896679f8a980aa9e5ff7
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-20 21:37:40 +0200

    Changed id type to Guid.

--- WalliCardsNet.API/Data/Models/FormData.cs
+++ WalliCardsNet.API/Data/Models/FormData.cs
@@ -2,7 +2,7 @@
 {
     public class FormData
     {
-        public string Id { get; set; } = new Guid().ToString();
+        public Guid Id { get; set; } = new Guid();
         public required string BusinessId { get; set; }
         public required string Email { get; set; }
 
