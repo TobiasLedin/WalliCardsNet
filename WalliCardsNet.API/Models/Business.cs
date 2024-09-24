ace2eafda90e3b0da396499317dab63bd8a18db5
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-24 16:07:33 +0200

    Added FieldDefinition (Customize what Customer data that Businesses want to retrieve and store).

---
+++ WalliCardsNet.API/Models/Business.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalliCardsNet.API.Models
+{
+    public class Business
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = "";
+        public string PspId { get; set; } = "";
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+        public List<CardTemplate> CardTemplates { get; set; } = [];
+        public List<FieldDefinition> FieldDefinitions { get; set; } = [];
+        public List<Customer> Customers { get; set; } = [];
+        public List<ApplicationUser> ApplicationUsers { get; set; } = []; // Managers and Employees with access to client application.
+    }
+
+}
---
+++ WalliCardsNet.API/Models/FieldDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WalliCardsNet.API.Models
+{
+    public class FieldDefinition
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [ForeignKey(nameof(Business))]
+        public Guid BusinessId { get; set; }
+
+
+        public string FieldName { get; set; } = string.Empty;   // Customer data fields (Name, Email, Adress etc)
+        public string FieldType { get; set; } = "string";   // Type of data (string, int/double etc)
+        public bool IsRequired { get; set; }
+    }
+}
