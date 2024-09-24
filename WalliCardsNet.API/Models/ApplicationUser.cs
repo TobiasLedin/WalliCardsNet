62f44035e90a2a53e7b87cc8a95c3a849efd3d75
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-24 16:09:39 +0200

    Namespace change.

---
+++ WalliCardsNet.API/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WalliCardsNet.API.Models
+{
+    // Custom user class extending IdentityUser
+    public class ApplicationUser : IdentityUser
+    {
+        [ForeignKey(nameof(Business))]
+        public Guid BusinessId { get; set; }
+        public Business? Business { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+
+    }
+}
