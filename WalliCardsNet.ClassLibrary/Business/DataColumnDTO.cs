2f60633415bdc9fb02bfd6833eb27b4360f96203
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-09-30 13:09:58 +0200

    More descriptive prop names

--- WalliCardsNet.ClassLibrary/Business/DataColumnDTO.cs
+++ WalliCardsNet.ClassLibrary/Business/DataColumnDTO.cs
@@ -2,7 +2,7 @@
 
 namespace WalliCardsNet.ClassLibrary.Business
 {
-    public record DataColumnDTO(string ColumnName, string ColumnType, bool IsRequired);
+    public record DataColumnDTO(string Key, string Title, string DataType, bool IsRequired);
     //{
     //    public string ColumnName { get; set; } = string.Empty;   // Customer data fields (Name, Email, Adress etc)
     //    public string ColumnType { get; set; } = "string";   // Type of data (string, int/double etc)
