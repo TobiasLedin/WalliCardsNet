b55a5f5bc5e0982c9802fd29e65dcdf3fdcd1dbf
qreedence <eden.yusof.ioannidis@gmail.com>
2024-10-01 15:00:09 +0200

    Add Stripe controller

---
+++ WalliCardsNet.API/Controllers/StripeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalliCardsNet.API.Data.Interfaces;
+
+namespace WalliCardsNet.API.Controllers
+{
+    [Route("/api/{controller}")]
+    [ApiController]
+    public class StripeController : ControllerBase
+    {
+        private readonly IBusiness _businessRepo;
+        public StripeController(IBusiness businessRepo)
+        {
+            _businessRepo = businessRepo;
+        }
+
+
+    }
+}
