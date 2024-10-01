08d78a318286b91b2a66e79fe9b4d74f65ff7d31
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-01 16:41:17 +0200

    Added empty Webhook controller.

---
+++ WalliCardsNet.API/Controllers/StripeWebhookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalliCardsNet.API.Controllers
+{
+    [Route("api/payment-webhook")]
+    [ApiController]
+    public class StripeWebhookController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Handle()
+        {
+
+            return Ok();
+        }
+
+    }
+}
