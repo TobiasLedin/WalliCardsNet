1e6694137e189fa5f854cc4e66c079ad7a41d038
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-07 20:11:41 +0200

    Added paymnet service provider name constants.

---
+++ WalliCardsNet.API/Constants/PaymentServiceProviders.cs
@@ -0,0 +1,8 @@
+﻿namespace WalliCardsNet.API.Constants
+{
+    public class PaymentServiceProviders
+    {
+        public const string Stripe = "Stripe";
+        public const string LemonSqueezy = "LemonSqueezy";
+    }
+}
