1d59e2ef6ef6b07b60a036ab271ed4d8ad4ba5ee
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-07 20:16:16 +0200

    Added GetMyIdAsync method overload (accepting string PSP ID). Added BusinessWithPspIdExists return a bool if business entity with PSP ID exists in DB.

--- WalliCardsNet.API/Data/Interfaces/IBusiness.cs
+++ WalliCardsNet.API/Data/Interfaces/IBusiness.cs
@@ -5,10 +5,12 @@ namespace WalliCardsNet.API.Data.Interfaces
     public interface IBusiness
     {
         public Task<Business> GetByIdAsync(Guid id);
+        public Task<Business> GetByIdAsync(string pspId);
         public Task<Business> GetByTokenAsync(string token);
         public Task<List<Business>> GetAllAsync();
         public Task AddAsync(Business business);
         public Task UpdateAsync(Business business);
         public Task RemoveAsync(Guid id);
+        public Task<bool> BusinessWithPspIdExists(string pspId);
     }
 }
