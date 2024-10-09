ce1d5858a848f573ea0ca248566526df3b12bbce
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-09 09:35:37 +0200

    Added author comments.

--- WalliCardsNet.API/Constants/Roles.cs
+++ WalliCardsNet.API/Constants/Roles.cs
@@ -1,5 +1,7 @@
 ﻿namespace WalliCardsNet.API.Constants
 {
+    // Author: Tobias
+    // Constants for Identity roles to avoid mistyping.
     public static class Roles
     {
         public const string Employee = "Employee";
