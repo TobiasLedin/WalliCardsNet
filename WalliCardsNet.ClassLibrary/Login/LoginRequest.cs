1b185a0c3be7b460d85c67b943ee4d86338733e4
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-24 10:36:59 +0200

    - Renamed DTO model. - Added validation attributes.

---
+++ WalliCardsNet.ClassLibrary/Login/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalliCardsNet.ClassLibrary.Login
+{
+    public record LoginRequest
+        (
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email adress")]
+        string Email,
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(25, MinimumLength = 8)]
+        string Password
+        );
+}
--- WalliCardsNet.ClassLibrary/Login/LoginRequestDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-
-namespace WalliCardsNet.ClassLibrary.Login
-{
-    public record LoginRequestDTO(string Email, string Password);
-}
