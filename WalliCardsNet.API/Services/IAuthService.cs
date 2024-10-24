fe9e273b811a587ba4defaf8da100add7963da28
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-24 10:56:14 +0200

    Renamed Login method.

--- WalliCardsNet.API/Services/IAuthService.cs
+++ WalliCardsNet.API/Services/IAuthService.cs
@@ -1,15 +1,14 @@
-﻿using WalliCardsNet.ClassLibrary.Login;
+﻿using WalliCardsNet.API.Models;
+using WalliCardsNet.ClassLibrary.Login;
 using WalliCardsNet.ClassLibrary.Register;
-using static WalliCardsNet.API.Services.APIAuthService;
+using static WalliCardsNet.API.Services.AuthService;
 
 namespace WalliCardsNet.API.Services
 {
     public interface IAuthService
     {
-
         Task<RegisterResponseDTO> CreateUserAccountAsync(Guid businessId, string role, string userName, string email);
-
-        Task<LoginResponseDTO> LoginAsync(string email, string password);
-        Task<LoginResponseDTO> LoginWithGoogleAsync(string code);
+        Task<AuthResult> AuthenticateUserAsync(string email, string password);
+        Task<AuthResult> LoginWithGoogleAsync(string code);
     }
 }
