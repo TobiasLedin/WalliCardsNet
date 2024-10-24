ad69acfd2654abcc708c7e1c02deebfc3bbd8a34
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-24 10:47:50 +0200

    - Now utilizes "AuthClient" HttpClient. - Added TokenRefresh method. - Modified Logout method to call Web API (refresh token deletion).

--- WalliCardsNet.Client/Services/ClientAuthService.cs
+++ WalliCardsNet.Client/Services/ClientAuthService.cs
@@ -1,9 +1,11 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
+using Sprache;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Json;
 using WalliCardsNet.ClassLibrary.Login;
+using WalliCardsNet.Client.Models;
 
 namespace WalliCardsNet.Client.Services
 {
@@ -15,30 +17,30 @@ namespace WalliCardsNet.Client.Services
 
         public ClientAuthService(IHttpClientFactory httpClientFactory, ILocalStorageService localStorage, AuthStateProvider authState)
         {
-            _httpClient = httpClientFactory.CreateClient("WalliCardsApi");
+            _httpClient = httpClientFactory.CreateClient("AuthClient");
             _localStorage = localStorage;
             _authState = authState;
         }
 
         public async Task LoginAsync(string email, string password)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/auth/login", new LoginRequestDTO(email, password));
+            var response = await _httpClient.PostAsJsonAsync("login", new LoginRequest(email, password));
 
             if (response.IsSuccessStatusCode)
             {
-                var result = await response.Content.ReadFromJsonAsync<LoginResponseDTO>() ?? throw new NullReferenceException("Unable to read Http response");
+                var result = await response.Content.ReadFromJsonAsync<LoginResponse>() ?? throw new NullReferenceException("Unable to read Http response");
 
-                if (result.Token != null)
+                if (result.AccessToken != null)
                 {
-                    await _localStorage.SetItemAsync("access-token", result.Token);
+                    await _localStorage.SetItemAsync("accessToken", result.AccessToken);
                     await _authState.GetAuthenticationStateAsync();
                 }
-            };
+            }
         }
 
         public async Task LinkGoogleAccountAsync(string code)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/auth/link/google", code);
+            var response = await _httpClient.PostAsJsonAsync("link/google", code);
             if (!response.IsSuccessStatusCode)
             {
                 throw new Exception("Something went wrong");
@@ -47,13 +49,13 @@ namespace WalliCardsNet.Client.Services
 
         public async Task LoginGoogleAsync(string code)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/auth/google", code);
+            var response = await _httpClient.PostAsJsonAsync("google", code);
             if (response.IsSuccessStatusCode)
             {
-                var result = await response.Content.ReadFromJsonAsync<LoginResponseDTO>();
-                if (result?.Token != null)
+                var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                if (result?.AccessToken != null)
                 {
-                    await _localStorage.SetItemAsync("access-token", result.Token);
+                    await _localStorage.SetItemAsync("accessToken", result.AccessToken);
                     await _authState.GetAuthenticationStateAsync();
                 }
             }
@@ -63,11 +65,44 @@ namespace WalliCardsNet.Client.Services
             }
         }
 
-        public async Task LogoutAsync()
+        public async Task<RefreshResult> RefreshAccessTokenAsync()
         {
-            await _authState.LogoutAsync();
+            try
+            {
+                var accessToken = await _localStorage.GetItemAsync<string>("accessToken");
 
+                if (!string.IsNullOrEmpty(accessToken))
+                {
+                    var response = await _httpClient.PostAsJsonAsync("refresh-token", accessToken);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+                        if (result?.AccessToken != null)
+                        {
+                            await _localStorage.SetItemAsync("accessToken", result.AccessToken);
+                            await _authState.GetAuthenticationStateAsync();
+
+                            return new RefreshResult { Success = true, Token = result.AccessToken };
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Unable to refresh token");
+            }
+
+            return new RefreshResult { Success = false };
         }
 
+        public async Task LogoutAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "logout");
+
+            await _httpClient.SendAsync(request);
+            await _authState.LogoutAsync();
+        }
     }
 }
