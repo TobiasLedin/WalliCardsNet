24b1cc6c86a67047a4eb54b01056db4de8e5ace2
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-24 10:53:59 +0200

    Added IncludeCredentialsHandler to include secure cookie with API calls.

---
+++ WalliCardsNet.Client/Handlers/IncludeCredentialsHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+namespace WalliCardsNet.Client.Handlers
+{
+    public class IncludeCredentialsHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+    }
+}
