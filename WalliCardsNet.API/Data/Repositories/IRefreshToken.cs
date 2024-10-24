d599977ef25b5f963925ef2f4390747571b65da5
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-10-24 11:05:09 +0200

    - Added RefreshToken model. - Added RefreshToken repository and interface.

---
+++ WalliCardsNet.API/Data/Repositories/IRefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using WalliCardsNet.API.Models;
+
+namespace WalliCardsNet.API.Data.Repositories
+{
+    public interface IRefreshToken
+    {
+        Task AddAsync(RefreshToken token);
+        Task RemoveAsync(string token);
+        Task<RefreshToken?> GetAsync(string tokenString);
+        Task<List<RefreshToken>> GetAllAsync();
+        Task RemoveExpiredAsync();
+    }
+}
---
+++ WalliCardsNet.API/Data/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using WalliCardsNet.API.Models;
+
+namespace WalliCardsNet.API.Data.Repositories
+{
+    public class RefreshTokenRepository : IRefreshToken
+    {
+        private readonly ApplicationDbContext _appDbCtx;
+
+        public RefreshTokenRepository(ApplicationDbContext appDbCtx)
+        {
+            _appDbCtx = appDbCtx;
+        }
+
+        public async Task AddAsync(RefreshToken token)
+        {
+            if (token != null)
+            {
+                await _appDbCtx.RefreshTokens.AddAsync(token);
+                await _appDbCtx.SaveChangesAsync();
+            }
+        }
+
+        public async Task<RefreshToken?> GetAsync(string tokenString)
+        {
+            return await _appDbCtx.RefreshTokens.FirstOrDefaultAsync(x => x.Token == tokenString);
+        }
+
+        public async Task<List<RefreshToken>> GetAllAsync()
+        {
+            return await _appDbCtx.RefreshTokens.ToListAsync();
+        }
+
+        public async Task RemoveAsync(string tokenString)
+        {
+            if (!string.IsNullOrWhiteSpace(tokenString))
+            {
+                var refreshToken = await _appDbCtx.RefreshTokens.FirstOrDefaultAsync(x => x.Token == tokenString);
+                if (refreshToken != null)
+                {
+                    _appDbCtx.RefreshTokens.Remove(refreshToken);
+                    await _appDbCtx.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task RemoveExpiredAsync()
+        {
+            await _appDbCtx.RefreshTokens.Where(x => x.Expiry < DateTime.UtcNow).ExecuteDeleteAsync();
+        }
+
+    }
+}
