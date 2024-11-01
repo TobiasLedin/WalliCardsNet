2c730aa09416aa666cdcabed798a5f1ba743deea
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-11-01 09:48:48 +0100

    Created basic model for storage of GoogleGenericClass related data.

---
+++ WalliCardsNet.API/Models/GooglePassTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace WalliCardsNet.API.Models
+{
+    public class GooglePassTemplate
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required Guid BusinessId { get; set; }
+        public required  string GoogleId { get; set; }
+        public required string IssuerId { get; set; }
+        public required string ClassId { get; set; }
+        public required string ObjectId { get; set; }
+
+        // Addition required props...
+    }
+}
