09c83d87e80c36b4129b4820f56f4f4898f7978c
qreedence <eden.yusof.ioannidis@gmail.com>
2024-11-04 14:44:01 +0100

    Refactor BusinessProfile DTOs

--- WalliCardsNet.API/Models/JoinForm.cs
+++ WalliCardsNet.API/Models/JoinForm.cs
@@ -3,6 +3,7 @@
     public class JoinForm
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        public required Guid BusinessProfileId { get; set; }
         public string FieldsJson { get; set; } = "{}";
         public string CSSOptionsJson { get; set; } = "{}";
     }
--- WalliCardsNet.ClassLibrary/BusinessProfile/JoinFormRequestDTO.cs
+++ WalliCardsNet.ClassLibrary/BusinessProfile/JoinFormRequestDTO.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace WalliCardsNet.ClassLibrary.BusinessProfile
 {
-    public class GooglePassTemplateRequestDTO
+    public class JoinFormRequestDTO
     {
-        public string LogoUrl { get; set; } = "";
-        public string HeroImage { get; set; } = "";
-        public string HexBackgroundColor { get; set; } = "#060000";
         public string FieldsJson { get; set; } = "{}";
+        public string CSSOptionsJson { get; set; } = "{}";
     }
 }
