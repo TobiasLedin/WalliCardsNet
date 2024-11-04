de18e89ff862f98759e2c4367dcab09ad4766399
qreedence <eden.yusof.ioannidis@gmail.com>
2024-11-04 09:11:47 +0100

    Initial implementation of google pass designer

---
+++ WalliCardsNet.ClassLibrary/BusinessProfile/BusinessProfileRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalliCardsNet.ClassLibrary.BusinessProfile
+{
+    public class BusinessProfileRequestDTO
+    {
+        public string LogoUrl { get; set; } = "";
+        public string HeroImage { get; set; } = "";
+        public string HexBackgroundColor { get; set; } = "#060000";
+        public GooglePassTemplateRequestDTO GooglePassTemplate { get; set; } = new GooglePassTemplateRequestDTO();
+    }
+}
