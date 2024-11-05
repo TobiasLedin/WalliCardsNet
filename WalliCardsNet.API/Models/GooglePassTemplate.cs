3f0b450d0831c86b96093013b82d26a6332aa2a2
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-11-05 22:04:03 +0100

    - Added props: CardTitle, Header, WideLogoUri. - Set imageUri props nullable. - Commented som props.

--- WalliCardsNet.API/Models/GooglePassTemplate.cs
+++ WalliCardsNet.API/Models/GooglePassTemplate.cs
@@ -4,15 +4,18 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public required Guid BusinessProfileId { get; set; }
-        public required  string GoogleId { get; set; }
+        public required string GoogleId { get; set; }
         public required string IssuerId { get; set; }
         public required string ClassId { get; set; }
-        public required string ObjectId { get; set; }
+        public required string ObjectId { get; set; } // TODO: Snacka om...
 
         // Addition required props...
+        public string CardTitle { get; set; } // Added
+        public string Header { get; set; } // Added
         public required string HexBackgroundColor { get; set; }
-        public required string LogoUri { get; set; }
-        public required string HeroImageUri { get; set; }
+        public string? LogoUri { get; set; } // Set optional
+        public string? WideLogoUri { get; set; } // Set optional
+        public string? HeroImageUri { get; set; } // Set optional
         public required string FieldsJson { get; set; }
     }
 }
